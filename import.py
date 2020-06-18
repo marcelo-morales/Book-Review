0887fbf918228e1882bf9d87fc11f15fd63b5d3b
Marcelo Morales <leonardofutcol@gmail.com>
2020-06-18 13:33:43 -0400

    Started up csv, implemented database

---
+++ import.py
@@ -0,0 +1,6 @@
+'''
+In a Python file called import.py separate from your web application, write a program that will take the books and import them into your
+PostgreSQL database. You will first need to decide what table(s) to create, what columns those tables should have, and how they should relate
+to one another. Run this program by running python3 import.py to import the books into your database, and submit this program with the rest of your 
+project code.
+'''
